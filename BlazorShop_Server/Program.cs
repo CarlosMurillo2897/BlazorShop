3440a39d09ef948be1afcf8327748ae381f31a4f
Carlos Murillo <cmb2897@gmail.com>
2022-06-21 20:45:03 -0600

    Added WASM Project Client to Solution. Showing Developer Exceptions page.

--- BlazorShop_Server/Program.cs
+++ BlazorShop_Server/Program.cs
@@ -18,6 +18,11 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    // If Development show exception errors.
+    app.UseDeveloperExceptionPage();
+}
 
 app.UseHttpsRedirection();
 
