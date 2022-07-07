2b8bd9dbeacfce2db73a2a6d0c4b881780ceb961
Carlos Murillo <cmb2897@gmail.com>
2022-07-07 15:11:40 -0600

    Updated BindProp Model. BindProp using Bootstrap classes, displaying elements in a more logical way, adding logic to show and modify Collections. Creating default array objects. Product using ProductProperties as property. ProductProperty Model created. Adding Models into Global Imports.

---
+++ BlazorShop_Models/Models/ProductProp.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BlazorShop_Models.Models
+{
+    public class ProductProp
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
