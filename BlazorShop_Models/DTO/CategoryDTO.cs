f4395c962b1fef47025df0b58f06f3db36ef3e4d
Carlos Murillo <cmb2897@gmail.com>
2022-08-31 14:29:35 -0600

    Adding Category List with DB data. Adding Required and Error Message for Category DTO properties. Updating Imports. Moving LifeCycle into Components folder. Adding Loading.gif for async purposes. Adding Category Upsert into order to create new Categories and edit older ones. Adding Category to Nav Menu items.

--- BlazorShop_Models/DTO/CategoryDTO.cs
+++ BlazorShop_Models/DTO/CategoryDTO.cs
@@ -1,6 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BlazorShop_Models.DTO {
     public class CategoryDTO {
          public int Id { get; set; }
+         [Required(ErrorMessage = "Please enter name.")]
          public string Name { get; set; }
     }
 }
